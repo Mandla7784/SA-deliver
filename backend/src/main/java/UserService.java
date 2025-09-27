9c005223b7aaf2e9459507d36c2dafce8f81e770
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-09-27 18:22:25 +0200

    added userService

---
+++ backend/src/main/java/UserService.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class UserService {
+
+
+
+public  void login(String name , String password) {
+    System.out.println("Login successful");
+}
+public  void  register(String name , String password){
+    System.out.println("User registered successfully");
+}
+}
