c80b748a2e9bdb077a4907fd18e8d3981f9180b4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-09-27 18:26:54 +0200

    working on test cases

--- backend/src/main/java/UserService.java
+++ backend/src/main/java/UserService.java
@@ -3,11 +3,35 @@ package main.java;
 public class UserService {
 
 
+    public  boolean login(String name, String password) {
+         return true;
 
-public  void login(String name , String password) {
-    System.out.println("Login successful");
-}
-public  void  register(String name , String password){
-    System.out.println("User registered successfully");
-}
+    }
+
+    public void register(String name, String password) {
+        System.out.println("User registered successfully");
+    }
+
+    public void viewProfile(String name) {
+        System.out.println("Viewing profile of " + name);
+
+    }
+
+    public void updateProfile(String name, String password) {
+        System.out.println("Updating profile of " + name);
+
+
+    }
+    public  void deleteProfile(String name){
+        System.out.println("Deleting profile of " + name);
+
+    }
+    public  void viewOrderHistory(String name){
+        System.out.println("Viewing order history of " + name);
+
+    }
+    public  void placeOrder(String name){
+        System.out.println("Placing order for " + name);
+
+    }
 }
